44115ccb0d29892126504cf5a909338950abd816
censorday <74760933+censorday@users.noreply.github.com>
2021-02-21 11:31:00 +0530

    Create wdsyl.php

---
+++ wdsyl.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+<title>
+AVstudy
+</title>
+</head>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200&display=swap');
+body{
+  background-color:#434C55;
+  font-family:'Cairo', sans-serif;
+}
+#top{
+  text-align:left;
+}
+h1{
+  color:white;
+  }
+h2{
+  color:white;
+  font-weight:100;
+}
+.sectop{
+  margin-top:10px;
+  margin-bottom:25px;
+  height:320px;
+  text-align:center;
+  color:white;
+}
+.intro{
+  color:white;
+  font-family:arial;
+  font-weight:100;
+  letter-spacing:15px;
+ text-align:center;
+}
+#name,#sml{
+  padding-top:7px;
+}
+.secmid{
+  padding-top:20px;
+  margin-top:30px;
+  margin-bottom:15px;
+  background-color:white;
+  min-height:450px;
+  text-align:center;
+  color:#434C55;
+  overflow:hidden;
+}
+.secbot{
+  padding-top:5px;
+  margin-top:10px;
+  margin-bottom:45px;
+  min-height:240px;
+  text-align:center;
+  
+}
+
+
+</style>
+
+<body>
+<div class="sectop">
+   <a style="text-decoration:none" href="https://avstudy.herokuapp.com/"><h2 id="top">AV-STUDY</h2></a>
+  <hr>
+  <br>
+  <h3 class="intro">WEB<br><BR> DEVELOPMENT </h3>
+  <div id="name"><hr  width="25%" size="1">
+   <h2 >By Aman Vashishth</h2></div>
+  <small id="sml">(Only 4th Sem Content is available here)</small>
+</div>      
+           
+<section class="secmid">  
+  <h3>Syllabus</h3>
+  <br>
+  <iframe src="https://avstudy.herokuapp.com/syl/syledm.pdf" type="application/pdf" width="96%" height="600px" />
+
+</section>
+
+</body>
+</html>
